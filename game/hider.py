f54aa3c9519ba94748550b65572731d6e86d5422
BelChaparro <a.belenchaparro@gmail.com>
2022-02-01 21:43:56 -0300

    completed game. updated seeker and removed spaces from comments in hider

--- game/hider.py
+++ game/hider.py
@@ -1,8 +1,7 @@
 import random
 
 class Hider:
-    """The person hiding from the Seeker. 
-    
+    """The person hiding from the Seeker.    
     The responsibility of Hider is to keep track of its location and distance from the seeker. 
     
     Attributes:
@@ -12,7 +11,6 @@ class Hider:
 
     def __init__(self):
         """Constructs a new Hider.
-
         Args:
             self (Hider): An instance of Hider.
         """
@@ -21,10 +19,8 @@ class Hider:
     
     def get_hint(self):
         """Gets a hint for the seeker.
-
         Args:
-            self (Hider): An instance of Hider.
-        
+            self (Hider): An instance of Hider.        
         Returns:
             string: A hint for the seeker.
         """
@@ -39,10 +35,8 @@ class Hider:
 
     def is_found(self):
         """Whether or not the hider has been found.
-
         Args:
-            self (Hider): An instance of Hider.
-            
+            self (Hider): An instance of Hider.            
         Returns:
             boolean: True if the hider was found; false if otherwise.
         """
@@ -50,7 +44,6 @@ class Hider:
         
     def watch_seeker(self, seeker):
         """Watches the seeker by keeping track of how far away it is.
-
         Args:
             self (Hider): An instance of Hider.
         """
